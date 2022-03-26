b8a0e3e11e97676bd4e78bba853aebf9cf67d726
Jiacheng Dong <2334173653@qq.com>
2022-03-26 18:24:10 +0800

    feat: add compose

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+import { compose } from './lib/Compose/index';
 import { LinkedList } from './lib/LinkedList/LinkedList';
 import { Queue } from './lib/LinkedList/Queue';
 import { promisify } from './lib/Promisify/index';
 
-export { promisify, LinkedList, Queue };
+export { promisify, LinkedList, Queue, compose };
---
+++ src/lib/Compose/index.ts
@@ -0,0 +1,10 @@
+type IMiddleware<T> = (arg0: T, ...args: unknown[]) => T;
+type ICompose<T> = (
+  middlewares: IMiddleware<T>[]
+) => (...extraArgs: unknown[]) => (initialValue: T) => T;
+
+export const compose: ICompose<number> =
+  middlewares =>
+  (...extraArgs) =>
+  initialValue =>
+    middlewares.reduce((accu, cur) => cur(accu, ...extraArgs), initialValue);
