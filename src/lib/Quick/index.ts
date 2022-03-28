5962fb48759d1afd2a944dc0eb4223dd0ff5050a
Jiacheng Dong <43509711+Jiacheng787@users.noreply.github.com>
2022-03-28 19:44:02 +0800

    Create index.ts

---
+++ src/lib/Quick/index.ts
@@ -0,0 +1,36 @@
+const swap = (nums: number[], i: number, j: number) => {
+  const temp = nums[i];
+  nums[i] = nums[j];
+  nums[j] = temp;
+};
+
+const partition = (nums: number[], lo: number, hi: number) => {
+  const pivot = nums[lo];
+  let i = lo + 1,
+    j = hi;
+  while (true) {
+    while (i < hi && nums[i] <= pivot) {
+      i++;
+    }
+    while (j > lo && nums[j] > pivot) {
+      j--;
+    }
+    if (i >= j) {
+      break;
+    }
+    swap(nums, i, j);
+  }
+  swap(nums, lo, j);
+  return j;
+};
+
+const _sort = (nums: number[], lo: number, hi: number) => {
+  if (lo >= hi) return;
+  const p = partition(nums, lo, hi);
+  _sort(nums, lo, p - 1);
+  _sort(nums, p + 1, hi);
+};
+
+const sort = (nums: number[]) => {
+  _sort(nums, 0, nums.length - 1);
+};
